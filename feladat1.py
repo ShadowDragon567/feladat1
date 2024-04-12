d7002c839ab24c35ffa4f95e35c60af72c6302c6
ShadowDragon567 <youremail@example.com>
2024-04-12 14:36:51 +0200

    Add project files.

---
+++ feladat1.py
@@ -0,0 +1,15 @@
+﻿import yfinance as yf
+import matplotlib.pyplot as plt
+
+# Bekérjük a felhasználótól a ticker nevét
+ticker = input("Adja meg a ticker nevét: ")
+
+# Lekérjük az adott ticker elmúlt egy évben történt ármozgásának adatait
+data = yf.download(ticker, period="1y")
+
+# Kirajzoljuk az ármozgást
+data['Adj Close'].plot(title=f"{ticker} részvény árának alakulása az elmúlt egy évben")
+plt.xlabel("Dátum")
+plt.ylabel("Ár (USD)")
+plt.show()
+
